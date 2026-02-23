50e6a1a7666127123473dab548cb9e6ab43a2c6d
vwffufgh-cellez <vwffufgh@gmail.com>
2026-02-23 02:46:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const mongoose = require('mongoose');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+// 🔌 الاتصال بقاعدة البيانات
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error("❌ Database Error:", err));
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`🤖 Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
-    if (message.content === '!ping') {
-        message.channel.send('Pong!');
-    }
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
 });
 
 client.login(process.env.TOKEN);
