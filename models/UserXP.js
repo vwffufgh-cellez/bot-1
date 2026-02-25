bb3d76a373dc6f1749d73a4db3f46dc92ffde7bd
vwffufgh-cellez <vwffufgh@gmail.com>
2026-02-25 09:20:54 -0800

    Update UserXP.js

--- models/UserXP.js
+++ models/UserXP.js
@@ -6,16 +6,15 @@ const userSchema = new mongoose.Schema({
 
   textXP: { type: Number, default: 0 },
   voiceXP: { type: Number, default: 0 },
-  level: { type: Number, default: 0 },
 
-  lastMessage: { type: Date, default: null },
-  history: [
-    {
-      amount: Number,
-      type: String, // text or voice
-      date: { type: Date, default: Date.now }
-    }
-  ]
+  weeklyTextXP: { type: Number, default: 0 },
+  weeklyVoiceXP: { type: Number, default: 0 },
+
+  monthlyTextXP: { type: Number, default: 0 },
+  monthlyVoiceXP: { type: Number, default: 0 },
+
+  level: { type: Number, default: 0 },
+  lastMessage: { type: Date, default: null }
 });
 
 module.exports = mongoose.model('UserXP', userSchema);
