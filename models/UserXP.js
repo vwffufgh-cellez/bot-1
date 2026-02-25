c08283506049877904841c8b961c6018ce36f1b7
vwffufgh-cellez <vwffufgh@gmail.com>
2026-02-25 04:22:16 -0800

    Update UserXP.js

--- models/UserXP.js
+++ models/UserXP.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 
-const userXPSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   guildId: String,
   userId: String,
 
-  xp: { type: Number, default: 0 },
+  textXP: { type: Number, default: 0 },
+  voiceXP: { type: Number, default: 0 },
+  level: { type: Number, default: 0 },
 
+  lastMessage: { type: Date, default: null },
   history: [
     {
       amount: Number,
+      type: String, // text or voice
       date: { type: Date, default: Date.now }
     }
   ]
 });
 
-module.exports = mongoose.model('UserXP', userXPSchema);
+module.exports = mongoose.model('UserXP', userSchema);
