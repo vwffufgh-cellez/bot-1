77dd1a5943ac62d6177cf87b5f0202a57a8ec0d6
vwffufgh-cellez <vwffufgh@gmail.com>
2026-02-25 03:53:45 -0800

    Create TicketClaim.js

---
+++ models/TicketClaim.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const ticketClaimSchema = new mongoose.Schema({
+  guildId: String,
+  channelId: String,
+  adminId: String,
+});
+
+module.exports = mongoose.model('TicketClaim', ticketClaimSchema);
