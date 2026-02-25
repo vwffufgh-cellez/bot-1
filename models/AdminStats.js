10bfec56127f4af9fff33ff34dec32f40513f516
vwffufgh-cellez <vwffufgh@gmail.com>
2026-02-25 03:54:27 -0800

    Update AdminStats.js

--- models/AdminStats.js
+++ models/AdminStats.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose');
 const adminStatsSchema = new mongoose.Schema({
   guildId: String,
   adminId: String,
-
-  ticketsCount: { type: Number, default: 0 },
-  xp: { type: Number, default: 0 },
-  warningsGiven: { type: Number, default: 0 },
-
-  warningsLevel: { type: Number, default: 0 },
-  rank: { type: Number, default: 1 }
+  ticketsClaimed: { type: Number, default: 0 },
+  xp: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('AdminStats', adminStatsSchema);
