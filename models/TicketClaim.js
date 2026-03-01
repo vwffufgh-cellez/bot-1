09168044e27037427a2ba4cc285e776b02b43b2e
vwffufgh-cellez <vwffufgh@gmail.com>
2026-03-01 02:54:28 -0800

    Update TicketClaim.js

--- models/TicketClaim.js
+++ models/TicketClaim.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const ticketClaimSchema = new mongoose.Schema({
-  guildId: String,
-  channelId: String,
-  adminId: String
+  guildId: { type: String, required: true },
+  channelId: { type: String, required: true, unique: true },
+  claimedById: { type: String, required: true },
+  claimedAt: { type: Date, default: Date.now }
 });
+
+ticketClaimSchema.index({ guildId: 1, channelId: 1 });
 
 module.exports = mongoose.model('TicketClaim', ticketClaimSchema);
