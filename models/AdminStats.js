c9f409cfefc6b9b4b694add1bd206080ef648bfa
vwffufgh-cellez <vwffufgh@gmail.com>
2026-03-02 09:15:06 -0800

    Update AdminStats.js

--- models/AdminStats.js
+++ models/AdminStats.js
@@ -4,6 +4,7 @@ const adminStatsSchema = new mongoose.Schema({
   guildId: String,
   adminId: String,
   ticketsClaimed: { type: Number, default: 0 },
+  claimsCount: { type: Number, default: 0 }, // توافق مع الكود القديم
   xp: { type: Number, default: 0 }
 });
 
