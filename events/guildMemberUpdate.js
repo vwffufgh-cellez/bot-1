9fc87925a2efa52c1ddd48ec4c981e79bd918df2
vwffufgh-cellez <vwffufgh@gmail.com>
2026-03-03 08:21:09 -0800

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,7 +5,6 @@ module.exports = {
   name: Events.GuildMemberUpdate,
   async execute(oldMember, newMember) {
     try {
-      // أي تحديث (خصوصًا رتب/اسم) يحدث البطاقة
       await upsertAdminProfile(newMember);
     } catch (e) {
       console.error('guildMemberUpdate profile sync error:', e);
