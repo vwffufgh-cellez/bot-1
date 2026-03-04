d39ca5737107ad2ca75ff273cede852bd080fe7f
vwffufgh-cellez <vwffufgh@gmail.com>
2026-03-04 07:32:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates
   ]
 });
